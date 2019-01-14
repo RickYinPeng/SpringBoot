b4038dd2c583b9feb6ec75476d991c3c4b9d5b65
RickYinPeng <272940172@qq.com>
2019-01-14 16:27:37 +0800

    SpringBoot学习第二天之配置

---
+++ spring_boot_02_config/src/main/java/com/yp/spring_boot/SpringBoot02ConfigApplication.java
@@ -0,0 +1,15 @@
+package com.yp.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@ImportResource(locations = {"classpath:beans.xml"})
+@SpringBootApplication
+public class SpringBoot02ConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot02ConfigApplication.class, args);
+	}
+
+}
+
