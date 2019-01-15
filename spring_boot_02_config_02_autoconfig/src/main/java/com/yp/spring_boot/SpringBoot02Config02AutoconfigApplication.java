deaebcdd05c7da8bcaed0a1b902ac398c914e923
RickYinPeng <272940172@qq.com>
2019-01-15 17:55:46 +0800

    SpringBoot学习第三天之自动配置

---
+++ spring_boot_02_config_02_autoconfig/src/main/java/com/yp/spring_boot/SpringBoot02Config02AutoconfigApplication.java
@@ -0,0 +1,13 @@
+package com.yp.spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot02Config02AutoconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot02Config02AutoconfigApplication.class, args);
+	}
+
+}
